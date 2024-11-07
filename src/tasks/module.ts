2d5ec306230ec8b8fb7abd93636d8b0d0491da4b
LeftTwixWand <0965142838gvs@gmail.com>
2024-11-07 13:07:44 +0100

    Remove npm install execution before the Tasks build

--- src/tasks/module.ts
+++ src/tasks/module.ts
@@ -49,25 +49,16 @@ export class TasksModule {
     }
 
     const terminal = vscode.window.activeTerminal ?? vscode.window.createTerminal('Azure Pipelines');
-    // Build the command sequence
 
     selectedTasks.forEach((task) => {
-      let commandSequence = `cd Tasks/${task} && npm install && cd.. && cd..`;
-      terminal.sendText(commandSequence);
-
       switch (action) {
         case 'build':
-          // terminal.sendText(`✅ Building task: ${task}\n`, false);
           terminal.sendText(`node make.js build --task ${task}`);
           break;
         case 'test':
-          // terminal.sendText(`🧪 Testing task: ${task}\n`, false);
           terminal.sendText(`node make.js test --task ${task}`);
           break;
         case 'deploy':
-          // terminal.sendText(`🚀 Deploying task: ${task}\n`, false);
-          // terminal.sendText(`Executing node make.js build --task ${task}`, false);
-
           terminal.sendText(`tfx build tasks delete --task-id 7B5A6198-ADF8-4B16-9939-7ADDF85708B2`);
           terminal.sendText(`tfx build tasks upload --task-path ./_build/Tasks/${task}`);
           break;
